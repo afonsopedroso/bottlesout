bdc9fb238cc9684d453f64c21310d93bb5ca02a6
Afonso <afonsonunespedroso@gmail.com>
2021-10-29 16:17:58 +0100

    first commit

---
+++ botlesout/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Bottlesout from './components/Bottlesout';
+
+function App() {
+  return (
+    <div className="App">
+      <Bottlesout />
+    </div>
+  );
+}
+
+export default App;
---
+++ botlesout/src/components/Bottlesout.js
@@ -0,0 +1,10 @@
+import React, { Component,useState } from 'react'
+
+
+function Bottlesout() {
+    return ( <div>
+        bottlesout
+    </div> );
+}
+
+export default Bottlesout;
